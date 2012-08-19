f2d228ef130180081ec93f94e7b0914e61e7a39f
Kevin Alford <zeroeth@pixelflow.org>
2012-08-18 22:44:37 -0500

    Parsing types, test skeleton

--- daerb_drac.rb
+++ daerb_drac.rb
@@ -1,7 +1,8 @@
 module Daerb
   class Card
     attr_accessor :name, :cost, :type, :power_and_toughness, :rules, :sets_and_rarity, :color, :loyalty
-    attr_accessor :main_type, :sub_type
+    attr_accessor :main_type, :sub_types
+    attr_accessor :sets
 
     def rules=(value)
       @rules = value.split(/\n/)
@@ -9,9 +10,13 @@ module Daerb
 
     def type=(value)
       @type = value
-      types = value.split(/  — /)
+      types = value.split(/ . /)
       self.main_type = types[0]
-      self.sub_type  = types[1]
+      self.sub_types = types[1].split if types[1]
+    end
+
+    def sets_and_rarity=(value)
+      self.sets = value.split(', ')
     end
   end
 end
---
+++ spec/card_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require './daerb_drac'
+
+describe Daerb::Card do
+  context 'parsing' do
+    describe '#sets_and_rarity' do
+      it 'gathers sets and their rarity'
+    end
+
+    describe '#power_and_toughness' do
+      it 'separates into two'
+      it 'handles halves'
+    end
+
+    describe '#color' do
+      it 'separates colors'
+    end
+
+    describe '#cost' do
+      it 'separates costs'
+      # into like colorless/w/b/b/g/r/x... how to handle phrexian? and
+      # either/ors
+    end
+  end
+end
